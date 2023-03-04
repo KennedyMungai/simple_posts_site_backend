a1ff9caa28ff8f9f8104cd48dfaf47554aa7cc41
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-04 11:19:20 +0300

    Rearranged the imports

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 """Created the python file that holds the logic for the models
 """
 import datetime
+
+import passlib.hash as hash
 import sqlalchemy
 import sqlalchemy.orm as orm
-import passlib.hash as hash
+
+import database

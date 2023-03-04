8f553bfc031a01804e9dbd22534711b67eb88c36
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-04 11:25:04 +0300

    Finished writing logic for the script

--- models.py
+++ models.py
@@ -3,11 +3,15 @@
 import datetime
 
 import passlib.hash as hash
-import sqlalchemy
 import sqlalchemy.orm as orm
+from sqlalchemy import Column, Table, Row, Integer, String, DateTime
 
 import database
 
 
 class UserModel(database.Base):
-    # ll
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow())

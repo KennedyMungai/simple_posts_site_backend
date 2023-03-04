78e5c29bd37df72a21581bd8b54872fc70792d38
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-04 12:25:21 +0300

    Deleted the unused import

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 """Created the file that holds the logic for the database file
     """
-import sqlalchemy
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
 from sqlalchemy import create_engine

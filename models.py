0dfb722c1fe3d497cf489f739db7cef77d5dfc59
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-04 12:14:49 +0300

    Added the name and phone columns

--- models.py
+++ models.py
@@ -12,6 +12,8 @@ from database import Base
 class UserModel(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    phone = Column(String)
     email = Column(String, unique=True, index=True)
     password_hash = Column(String)
     created_at = Column(DateTime, default=datetime.datetime.utcnow())

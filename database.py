0305908f88305404018e0dcc366e888b6a37b3e4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-04 10:09:26 +0300

    Imported the load_dotenv function from dotenv and almost finished the DB_URL variable

--- database.py
+++ database.py
@@ -1,3 +1,9 @@
 import sqlalchemy as sqlalchemy
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = 'mysql+pymysql://'

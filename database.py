db57f40be2325b679d29241c01db902070f63d0f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-04 10:20:30 +0300

    Made some changes

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
-import sqlalchemy as sqlalchemy
+import sqlalchemy
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
-import os
+from os import getenv
+from sqlalchemy import create_engine
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_URL = 'mysql+pymysql://'
+DB_URL = getenv('MYSQL_PATH')
+
+engine = create_engine(DB_URL)

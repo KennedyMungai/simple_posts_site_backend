87719ec5094fb931843a7e57bc7d3b293a3bfcf6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 11:55:54 +0300

    Called the get_user_by_email async function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """This is the new entry point for the application"""
 from fastapi import FastAPI, Security as _security, Depends
 from sqlalchemy import orm as _orm
-from services import get_db
+from services import get_db, get_user_by_email
 
 from schemas import UserRequest
 
@@ -9,6 +9,6 @@ app = FastAPI()
 
 
 @app.post('/api/v1/users')
-async def register_user(user: UserRequest, db: _orm.Session = Depends(get_db())):
+async def register_user(user: UserRequest, _db: _orm.Session = Depends(get_db())):
     """The API endpoint for user creation in the application"""
-    pass
+    db_user = await get_user_by_email(email=user.email, db=_db)

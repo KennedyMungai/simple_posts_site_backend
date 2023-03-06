9bc9666e90d657e8a5e11108b16617682114f010
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 15:12:23 +0300

    Finished writing the register user endpoint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import orm as _orm
 
 from schemas import UserRequest
-from services import get_db, get_user_by_email
+from services import get_db, get_user_by_email, create_user, create_token
 
 app = FastAPI()
 
@@ -18,3 +18,8 @@ async def register_user(user: UserRequest, _db: _orm.Session = Depends(get_db())
             status_code=400,
             detail="Email already exists, try with another email address"
         )
+
+    # create the user and return a token
+    db_user = await create_user(user, _db)
+
+    return await create_token(db_user)

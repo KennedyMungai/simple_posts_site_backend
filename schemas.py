67c771a75cab5d811b04850261408e4b7e604778
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 10:58:16 +0300

    Defined the UserBase class

--- schemas.py
+++ schemas.py
@@ -1,4 +1,15 @@
 """A file that holds all the schema data
 """
-import pydantic as _pydantic
+from pydantic import BaseModel
 import datetime as _datetime
+
+
+class UserBase(BaseModel):
+    """The UserBase class
+
+    Args:
+        BaseModel (class): The parent class for the UserBase class
+    """
+    email: str
+    name: str
+    phone: str

1f6d4fa1c93d25137b8f21df2c883e74eae260b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 10:23:09 +0300

    Wrote the create_db function

--- services.py
+++ services.py
@@ -1,3 +1,8 @@
 """This script contains the services code
 """
 import database as _database
+
+
+def create_db():
+    """This method calls the database creation method"""
+    return _database.Base.metadata.create_all(bind=_database.engine)

6b0135748e0d2bba579c95e185615ee1ccc9975b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 12:00:18 +0300

    Raised a HttpException if the email already exists in the database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """This is the new entry point for the application"""
-from fastapi import FastAPI, Security as _security, Depends
+from fastapi import FastAPI, Security as _security, Depends, HTTPException
 from sqlalchemy import orm as _orm
 from services import get_db, get_user_by_email
 
@@ -11,4 +11,10 @@ app = FastAPI()
 @app.post('/api/v1/users')
 async def register_user(user: UserRequest, _db: _orm.Session = Depends(get_db())):
     """The API endpoint for user creation in the application"""
-    db_user = await get_user_by_email(email=user.email, db=_db)
+    db_user = await get_user_by_email(email: user.email, db: _db)
+
+    if db_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists, try with another email address"
+        )

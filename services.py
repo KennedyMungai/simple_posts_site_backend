a881f947de7569454e4d524311a71d9f137881d6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 10:31:43 +0300

    Called the create_db function

--- services.py
+++ services.py
@@ -6,3 +6,6 @@ import database as _database
 def create_db():
     """This method calls the database creation method"""
     return _database.Base.metadata.create_all(bind=_database.engine)
+
+
+create_db()

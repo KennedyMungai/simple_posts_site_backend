c2d8c0417c37970080b0f42989874af5bff23cb1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 10:34:33 +0300

    Got rid pf the unused impoort for the mysql URL

--- database.py
+++ database.py
@@ -4,8 +4,6 @@ import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
 from sqlalchemy import create_engine
 
-from keys import MYSQL_KEY
-
 DB_URL = "sqlite:///./dbfile.db"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})

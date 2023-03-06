b6e49d0b7eff91a54d0fcf41f8bdb61a972f621a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 11:24:14 +0300

    CReated a database session

--- services.py
+++ services.py
@@ -6,3 +6,12 @@ import database as _database
 def create_db():
     """This method calls the database creation method"""
     return _database.Base.metadata.create_all(bind=_database.engine)
+
+
+def get_db():
+    db = _database.SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()

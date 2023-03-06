9c46219c2de33bda958a5222e97ee6bb463175dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-06 11:40:57 +0300

    CReated the register_user api endpont

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 """This is the new entry point for the application"""
-from fastapi import FastAPI, Security as _security
+from fastapi import FastAPI, Security as _security, Depends
 from sqlalchemy import orm as _orm
+from services import get_db
+
+from schemas import UserRequest
 
 app = FastAPI()
+
+
+@app.post('/api/v1/users')
+async def register_user(user: UserRequest, db: _orm.Session = Depends(get_db())):
+    """The API endpoint for user creation in the application"""
+    pass
